23ee8cbd766ddc35b438df7aa7a224cca93988bb
Taimoor Ghori <taimoor@slash.co>
2025-11-10 20:51:43 +0700

    fix error

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
 "use client";
 
-const REPORT_URL =
-  "https://lookerstudio.google.com/embed/reporting/ba317d17-d45b-470f-9ea9-d4ba5be5e940/page/tEnnC";
+import LookerReport from "../components/LookerReport";
+import { usePathname } from 'next/navigation';
 
 export default function Page() {
+  const pathname = usePathname();
   const currentYear = new Date().getFullYear();
   const years = Array.from({ length: currentYear - 1990 + 1 }, (_, i) => currentYear - i);
 
 
   return (
-    <div className="dashboard-container">
-
+    <>
       {/* Main Section */}
       <main className="main-content">
         {/* Section Header + Button */}
@@ -26,17 +26,8 @@ export default function Page() {
         </div>
 
         {/* Iframe Container */}
-        <div className="iframe-container">
-          <iframe
-            title="Looker Studio Report"
-            src={REPORT_URL}
-            frameBorder="0"
-            allowFullScreen
-            sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
-          ></iframe>
-        </div>
+        <LookerReport key={pathname} />
       </main>
-
-    </div>
+    </>
   );
 }
---
+++ components/LookerReport.js
@@ -0,0 +1,17 @@
+"use client";
+
+const REPORT_URL = "https://lookerstudio.google.com/embed/reporting/ba317d17-d45b-470f-9ea9-d4ba5be5e940/page/tEnnC";
+
+export default function LookerReport() {
+  return (
+    <div className="iframe-container">
+      <iframe
+        title="Looker Studio Report"
+        src={REPORT_URL}
+        frameBorder="0"
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      ></iframe>
+    </div>
+  );
+}
